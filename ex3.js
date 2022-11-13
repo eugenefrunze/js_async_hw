e60deb17ba20008f0ac7b9e4805c6f09828e399f
Eugene <miloslavsky9000@gmail.com>
2022-11-13 22:49:49 +0200

    ex3 solved

--- ex3.js
+++ ex3.js
@@ -1,7 +1,11 @@
 // ex. 3
 // add error handling for fetch (based on statuses, ok property)
 
-// fetch('https://jsonplaceholder.typicode.com/post')
-//   .then(res => {
-//     // here should go the logic
-//   })
+fetch('https://jsonplaceholder.typicode.com/post')
+// fetch('https://dummyjson.com/products/1')
+  .then(res => {
+    if(!res.ok) throw new Error(`Error fetching, status is ${res.status}`)
+    return res.json();
+  })
+  .then((json) => console.log(json))
+  .catch(error => console.error(error.message));
