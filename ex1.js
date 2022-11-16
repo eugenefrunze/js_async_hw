56f1290424f89e04aaffd7dfb3f782f57895621e
Eugene <miloslavsky9000@gmail.com>
2022-11-16 12:23:29 +0200

    ex4 resolved

--- ex1.js
+++ ex1.js
@@ -1,3 +1,12 @@
 // ex. 1
 // Promise['Method'] -> ~ all that always returns either it's success or error
 // [ 'success', 'error' ]
+
+Promise.allSettled([
+        Promise.resolve('success'),
+        Promise.reject('error'),
+        Promise.resolve('success'),
+    ])
+    .then(results => {
+        console.log(results.map((el) => el?.value ? el.value : el.reason))
+    })
--- ex4.js
+++ ex4.js
@@ -4,14 +4,30 @@
 
 // you should pass a callback to it, and arguments to that callback
 
-// const promisify = (functionAsCallback, ...argsToFunction) => {
-//
-//   // your code should go here
-//
-// }
+const promisify = (functionAsCallback, ...argsToFunction) => {
+    return function (...argsToFunction) {
+        return new Promise((resolve, reject) => {
+            function callback(error, result) {
+                if (error) {
+                    return reject(error)
+                } else {
+                    return resolve(result)
+                }
+            }
+            argsToFunction.push(callback)
+            functionAsCallback.call(this, ...argsToFunction)
+        })
+    }
+}
 
-// it should return a promise, so you can call .then on this function
-// const promisifiedFunction = promisify(someAsyncFunction, [args])
-// .then(result => {
-//   console.log(result)
-// })
+const someAsyncFunction = (num1, num2, callback) => {
+    if (!num1 || !num2) {
+        return callback(new Error('truobles with arguments'), null)
+    }
+    return callback(null, num1 + num2)
+}
+
+const promisifiedFunction = promisify(someAsyncFunction)
+
+
+promisifiedFunction(6, 2).then(result => console.log(result))
