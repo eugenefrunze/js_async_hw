b567dee66ef8bda1ff7ce2a1a7efa4fe0997e166
Eugene <miloslavsky9000@gmail.com>
2022-11-16 12:29:50 +0200

    ex4 for review

--- ex4.js
+++ ex4.js
@@ -4,6 +4,18 @@
 
 // you should pass a callback to it, and arguments to that callback
 
+// const promisify = (functionAsCallback, ...argsToFunction) => {
+//
+//   // your code should go here
+//
+// }
+
+// it should return a promise, so you can call .then on this function
+// const promisifiedFunction = promisify(someAsyncFunction, [args])
+// .then(result => {
+//   console.log(result)
+// })
+
 const promisify = (functionAsCallback, ...argsToFunction) => {
     return function (...argsToFunction) {
         return new Promise((resolve, reject) => {
